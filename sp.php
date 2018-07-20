83c210c746392d9c0108e026d398f824d740404e
Mukesh Singh <firemoonstudiosdeveloper@gmail.com>
2018-07-20 17:34:35 +0530

    43

--- sp.php
+++ sp.php
@@ -4,7 +4,7 @@
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
     
-    include("../SharePointAPI.php");
+    include("SPAPI/SharePointAPI.php");
 
 
     use Thybag\SharePointAPI;
