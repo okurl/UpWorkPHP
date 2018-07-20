5c96058a0c99ff5312c9c1f4e43c1b03f1456d3e
Mukesh Singh <firemoonstudiosdeveloper@gmail.com>
2018-07-20 17:08:47 +0530

    43

--- sp.php
+++ sp.php
@@ -9,28 +9,18 @@
     $token = 'Bearer '.$_GET['token'];
     
     $url = "https://tatacommunications.sharepoint.com/sites/projectmarketplace_v1/_api/web/lists/GetByTitle('UpWorkTokens')/items?\$select=*&\$filter=EmployeeName/ID eq 10330";
-    
-    $resource = curl_init($url);
 
-    $headers = array(
-        "Accept" => "application/json;odata=verbose",
-        "crossDomain"=>"true",
-        "Authorization"=>$token
-    );
-
-    curl_setopt($resource, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($resource, CURLOPT_CUSTOMREQUEST, 'GET');
+    $request_headers = array();
+    $request_headers[] = 'Accept: application/json;odata=verbose';
+    $request_headers[] = 'Authorization:'.$token;
 
 
+    // Performing the HTTP request
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $request_headers);
+    $response_body = curl_exec($ch);
 
-    // Send the request & save response to $resp
-    $resp = curl_exec($resource);
-    // Close request to clear up some resources
-
-    echo $resp;
-
-    curl_close($resource);
-
+    echo $response_body;
 
 
 
