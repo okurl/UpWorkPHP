06c6174e35a17a973a8c35372d06df1972069dea
Mukesh Singh <firemoonstudiosdeveloper@gmail.com>
2018-07-20 17:31:09 +0530

    spiapi

--- sp.php
+++ sp.php
@@ -1,32 +1,11 @@
 <?php
+    include("../SharePointAPI.php");
 
 
+    use Thybag\SharePointAPI;
+    $sp = new SharePointAPI('projectmarketplace@tatacommunications.com', 'Tata@2016', 'SPAPI/Lists.xml');
 
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+    $resp = $sp->read('UpWorkTokens');
 
-    $token = 'Bearer '.$_GET['token'];
-    
-    $url = "https://tatacommunications.sharepoint.com/sites/projectmarketplace_v1/_api/web/lists/GetByTitle('UpWorkTokens')/items?\$select=*&\$filter=EmployeeName/ID eq 10330";
-
-    $request_headers = array();
-    $request_headers[] = 'Accept: application/json;odata=verbose';
-    $request_headers[] = 'Authorization:'.$token;
-
-
-    // Performing the HTTP request
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $request_headers);
-    $response_body = curl_exec($ch);
-
-    echo $response_body;
-
-
-
-
-
-
-
-
+    echo $resp;
 ?>
