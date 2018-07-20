b70cd32e4e6206685d4bdb24fd401c28d1490097
Mukesh Singh <firemoonstudiosdeveloper@gmail.com>
2018-07-20 15:09:53 +0530

    4567

--- sp.php
+++ sp.php
@@ -1,4 +1,9 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $token = 'Bearer '.$_GET['token'];
 $url = "https://tatacommunications.sharepoint.com/sites/projectmarketplace_v1/_api/web/lists/GetByTitle('UpWorkTokens')/items?$select=*&$filter=EmployeeName/ID eq 10330";
 $resource = curl_init($url);
