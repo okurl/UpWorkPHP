f6049481ab195ce6e8d34f708e85b78612cda2a0
Mukesh Singh <firemoonstudiosdeveloper@gmail.com>
2018-11-15 18:58:23 +0530

    new change

--- index.php
+++ index.php
@@ -32,13 +32,18 @@
 
 // Our php-oauth library - used in this example - requires a session
 session_start();
-error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 if(!empty($_GET['userId'])){
     $_SESSION['userId'] = $_GET['userId'];
     $_SESSION['sitetoken'] = $_GET['sitetoken'];
 }
 else{
     if(isset($_SESSION['userId'])){
+
+    }
+    else{
+        header('Location: https://www.upwork.com/ab/jobs-home/4810206');
+    }
+}
 ?>
     <script>
         //clear local storage
@@ -100,11 +105,6 @@ $auth = new \Upwork\API\Routers\Auth($client);
 $info = $auth->getUserInfo();
 
 print_r($info);
-
-    }
-    else{
-        header('Location: https://www.upwork.com/ab/jobs-home/4810206');
-    }
 ?>
 </body>
 </html>
